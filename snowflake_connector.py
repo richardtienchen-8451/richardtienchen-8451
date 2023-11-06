dc6a482006b346b781d61a0297a7e7d102c3038d
Richard Tien Chen <richardtien.chen@8451.com>
2023-11-06 11:40:42 -0500

    add connection

--- snowflake_connector.py
+++ snowflake_connector.py
@@ -7,3 +7,20 @@ from datetime import date
 
 # scikit-learn (install: pip install -U scikit-learn)
 from sklearn.linear_model import LinearRegression
+
+
+connection_parameters = {
+  "ACCOUNT":"eighty451.east-us-2.azure",
+  "USER":"SVC_STRATUM_STAGING_SOURCE_MEASUREMENTS_RO",
+  "PASSWORD":"???",
+  "ROLE":"STRATUM_STAGING_DEV_SOURCE_MEASUREMENTS_RO_AR",
+  "DATABASE":"STRATUM_STAGING_DEV",
+  "SCHEMA":"SOURCE_MEASUREMENTS",
+  "WAREHOUSE":"STRATUM_STAGING_ADHOC_WH"
+}
+
+sess = Session.builder.configs(connection_parameters).create()
+
+df = sess.sql("select current_role(), current_database(), current_schema(), current_warehouse()")
+
+df.show()
